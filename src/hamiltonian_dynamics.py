febdc2cd0a92d40d30bc95d701c04d6ffde45aa6
adrianlopezlanchares <202107370@alu.comillas.edu>
2025-11-25 14:58:11 +0100

    create hamilton sampling function

--- src/hamiltonian_dynamics.py
+++ src/hamiltonian_dynamics.py
@@ -53,7 +53,13 @@ def H(x: np.ndarray, p: np.ndarray, M: np.ndarray, f: Callable) -> float:
 
 
 def compute_alpha(
-    x: np.ndarray, p: np.ndarray, M: np.ndarray, L: int, f: Callable
+    x_L: np.ndarray,
+    p_L: np.ndarray,
+    x_0: np.ndarray,
+    p_0: np.ndarray,
+    M: np.ndarray,
+    L: int,
+    f: Callable,
 ) -> float:
     """Compute the acceptance ratio for the next sample
 
@@ -68,8 +74,8 @@ def compute_alpha(
         float: acceptance ratio of next sample
     """
 
-    hamiltonian_L = H(x[L], p[L], M, f)
-    hamiltonian_0 = H(x[0], p[0], M, f)
+    hamiltonian_L = H(x_L, p_L, M, f)
+    hamiltonian_0 = H(x_0, p_0, M, f)
 
     numerator = np.exp(-hamiltonian_L)
     denominator = np.exp(-hamiltonian_0)
@@ -104,8 +110,46 @@ def leapfrog(
     p_new = x.copy()
 
     for t in range(L):
-        p_halfstep = p[t] - (step_size / 2) * grad_U(x, f, grad_f)
-        x_new = x[t] + step_size * np.linalg.inv(M) * p_halfstep
+        p_halfstep = p - (step_size / 2) * grad_U(x, f, grad_f)
+        x_new = x + step_size * np.linalg.inv(M) * p_halfstep
         p_new = p_halfstep - (step_size / 2) * grad_U(x, f, grad_f)
 
     return x_new, p_new
+
+
+def hamiltonian_sample(
+    f: Callable, grad_f: Callable, num_samples: int, L: int = 100, step_size: int = 1
+) -> np.ndarray:
+    """Sample points using hamiltonian-montecarlo
+
+    Args:
+        f: Function you want to sample from
+        grad_f: Gradient of the function you want to sample from
+        num_samples: Number of samples to generate
+        L: Number of leapfrog steps
+        step_size: Size of each timestep
+
+    Returns:
+        np.ndarray: Array of the sampled points
+    """
+
+    M = np.eye(2)  # initial value of mass matrix (symmetric and positive definite)
+    x = np.ndarray([1, 2])  # initial value (must be same dimension as f input)
+    p = np.random.normal(0, M)  # initial value (sample random momentum)
+
+    samples = np.empty((0, 2))
+
+    for i in range(num_samples):
+        x_new, p_new = leapfrog(x, p, M, L, step_size, f, grad_f)
+
+        acceptance_rate = compute_alpha(x_new, p_new, x, p, M, L, f)
+
+        if np.random.random() < acceptance_rate:
+            samples = np.vstack([samples, x_new])
+
+            x = x_new
+            p = p_new
+        else:
+            samples = np.vstack([samples, x])
+
+    return samples
